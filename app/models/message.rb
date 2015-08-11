1d2fdab7973e668e5eab3574055f95076708c4aa
mitsukura <mitsukura@gmail.com>
2015-08-11 13:14:18 +0000

    challenge2_retry

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,13 @@ class Message < ActiveRecord::Base
     # 名前は必須入力かつ20文字以内
     validates :name , length: {  maximum: 20 } , presence: true
     # 内容は必須入力かつ100歳未満
-    validates :age , length: {minimum: 0 , maximum: 2 } , presence: true    
+    validates :age ,
+                numericality: {
+                    only_integer:true,
+                    greater_than_or_equal_to:1,
+                    less_than_or_equal_to:130
+                }
+                
     # 内容は必須入力かつ2文字以上30文字以下
     validates :body , length: {minimum: 2 , maximum: 30 } , presence: true
 end
